f734c42219686fdbfd289ac400f1520176d52f3a
Vlado Zilka <vlado.zilka@gmail.com>
2018-07-17 17:19:10 +0200

    rename source to transaction_source in resource

--- src/Http/Resources/TransactionResource.php
+++ src/Http/Resources/TransactionResource.php
@@ -17,18 +17,18 @@ class TransactionResource extends Resource
     {
         /** @var \Unite\Transactions\Models\Transaction $this->resource */
         return [
-            'id'                        => $this->id,
-            'type'                      => $this->type,
-            'amount'                    => $this->amount,
-            'balance'                   => $this->balance,
-            'variable_symbol'           => (int) $this->variable_symbol,
-            'specific_symbol'           => (int) $this->specific_symbol,
-            'description'               => $this->description,
-            'posted_at'                 => (string)$this->posted_at,
-            'created_at'                => (string)$this->created_at,
-            'destination_iban'          => $this->destination_iban,
-            'source'                    => new SourceResource($this->source),
-            'tags'                      => TagResource::collection($this->tags),
+            'id'                 => $this->id,
+            'type'               => $this->type,
+            'amount'             => $this->amount,
+            'balance'            => $this->balance,
+            'variable_symbol'    => (int)$this->variable_symbol,
+            'specific_symbol'    => (int)$this->specific_symbol,
+            'description'        => $this->description,
+            'posted_at'          => (string)$this->posted_at,
+            'created_at'         => (string)$this->created_at,
+            'destination_iban'   => $this->destination_iban,
+            'transaction_source' => new SourceResource($this->source),
+            'tags'               => TagResource::collection($this->tags),
         ];
     }
 }
